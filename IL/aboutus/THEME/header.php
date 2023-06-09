f678b7309fe6d74bb82b0c67d162bae96185a040
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2023-06-08 18:54:04 -0700

    Create header.php
    Header Code

---
+++ IL/aboutus/THEME/header.php
@@ -0,0 +1,15 @@
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide" style="padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:site-title {"level":0} /-->
+
+<!-- wp:paragraph -->
+<p><a href="https://www.iterationlinc.com/">IterationLinc</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:navigation {"ref":82} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:image {"align":"full","sizeSlug":"large"} -->
+<figure class="wp-block-image alignfull size-large"><img src="https://www.iterationlinc.com/assets/img/aboutus-image/header.jpg" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
